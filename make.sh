fb1c9125610ec4c8f310a710fee57d8ee0f85949
Rioboyva2554 <116155137+Rioboyva2554@users.noreply.github.com>
2025-03-03 21:16:00 +1100

    No worky yet

---
+++ make.sh
@@ -0,0 +1,8 @@
+nasm -f bin bootloader.s -o bootloader.bin
+nasm -f bin finkernel.s -o finkernel.bin
+sudo dd if=/dev/zero of=test.img bs=512 count=2880
+sudo chmod 7777 test.img
+dd if=bootloader.bin of=test.img conv=notrunc
+dd if=finkernel.bin of=test.img seek=1 conv=notrunc
+qemu-system-i386 test.img
+
